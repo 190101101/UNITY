041dceea29ad719757d2e6ce01490725ef2671c5
190101101 <190101101@mail.ru>
2025-01-19 00:08:41 +0400

    08 DeğişkenlerTanımlamaTürleri

---
+++ dev/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dev
+{
+  class Test
+  {
+    static void Main(string[] args)
+    {
+
+      //? string - text data
+      //? int - number data
+      //? float - number data 3.1111 32 byte
+      //? double - big data 3.23123312 [50] 60 byte
+      //? bool - true false
+
+      string p1 = "hello world";
+      int p2 = 19;
+      float p3 = 3.6341f;
+      double p4 = 3.54654;
+      bool p5 = false;
+
+      Console.WriteLine(p1);
+      Console.WriteLine(p2);
+      Console.WriteLine(p3);
+      Console.WriteLine(p4);
+      Console.WriteLine(p5);
+
+    }  
+  }
+}
