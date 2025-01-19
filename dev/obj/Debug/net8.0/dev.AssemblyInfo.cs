9f9621e5caa48692e6c1a9ae288ab68e6d294eba
190101101 <190101101@mail.ru>
2025-01-19 17:06:43 +0400

    11 Array

--- dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
+++ dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080becdeb65df285d0630c3f231a46ca346d8e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed22f5ea1cbf631c35cf610ba740cbe05c8233a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
