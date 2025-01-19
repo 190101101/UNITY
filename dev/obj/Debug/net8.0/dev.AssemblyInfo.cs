4da4b0c0391d9d7c37a1d10c911982d49b74555c
190101101 <190101101@mail.ru>
2025-01-19 17:13:12 +0400

    14 Switch

--- dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
+++ dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bbe915796356df03fec4a6074f9899719e4888")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f99f3ab74bd2d46041820a9afc2a14f51890b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
