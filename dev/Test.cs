c90b533b8f2c4ca95b09db7327ac1a94b89aa118
190101101 <190101101@mail.ru>
2025-01-20 12:39:34 +0400

    17 İLERİ SEVİYE

--- dev/Test.cs
+++ dev/Test.cs
@@ -6,21 +6,6 @@ namespace dev
   {
     static void Main(string[] args)
     {
-      string taskagitmakas(string bir, string iki) => (bir, iki) switch
-      {
-        ("tas", "kagit") => "kagit kazandi",
-        ("tas", "makas") => "tas kazandi",
-        ("kagit", "tas") => "kagit kazandi",
-        ("kagit", "makas") => "makas kazandi",
-        ("makas", "tas") => "tas kazandi",
-        ("makas", "kagit") => "makas kazandi",
-        ("tas", "tas") => "beraber",
-        ("makas", "makas") => "beraber",
-        ("kagit", "kagit") => "beraber",
-        (_, _) => "degerler bosh"
-      };
-
-      Console.WriteLine(taskagitmakas("tas", "makas"));
     }
   }
 }
--- dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
+++ dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1277737e1d02f0ccdea15c6f25573d726ea43a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a1be166d6f077e3df56484899cb10496af003c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
