33d09cfafd16160443ab4f7ab4b2db6646b1cd44
190101101 <190101101@mail.ru>
2025-01-22 14:45:24 +0400

    37 GetComponent Anlatımları ve işlemleri 1

---
+++ base/06 UNİTYYE İLK ADIMLAR/37 GetComponent Anlatımları ve işlemleri 1/ilkdosyam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ilkdosyam : MonoBehaviour
+{
+    CapsuleCollider Capsulum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Capsulum = GetComponent<CapsuleCollider>();
+        Capsulum.enabled = false;
+        Capsulum.isTrigger = true;
+        Capsulum.height = 7;
+
+        CapsuleCollider[] capsullerim = GetComponents<CapsuleCollider>();
+
+        for(int i = 0; i < capsullerim.Length; i++)
+        {
+            Debug.Log(capsullerim[i]);
+            capsullerim[i].enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
