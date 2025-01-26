b04d57f1e154b86fa7001fef4653c8f24c12c243
190101101 <190101101@mail.ru>
2025-01-26 04:15:12 +0400

    47 Transform Translate Vector3 Static Özellikler 1

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/47 Transform Translate Vector3 Static Özellikler 1/Script/transformislemleri.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformislemleri : MonoBehaviour
+{
+    Vector2 a;
+    Vector3 b;
+    Vector4 c;
+    Quaternion d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("position:"+transform.position);
+        Debug.Log("rotation:"+transform.rotation);
+        Debug.Log("scale:"+transform.localScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.one * Time.deltaTime);
+        // transform.Translate(Vector3.zero * Time.deltaTime); ?
+        // transform.Translate(Vector3.up * Time.deltaTime);
+        // transform.Translate(Vector3.back * 5f * Time.deltaTime);
+        // transform.Translate(new Vector3(0f,1f,0f) * Time.deltaTime);
+        // transform.Translate(new Vector3(0f,1f,0f) * 5f * Time.deltaTime);
+    }
+}
