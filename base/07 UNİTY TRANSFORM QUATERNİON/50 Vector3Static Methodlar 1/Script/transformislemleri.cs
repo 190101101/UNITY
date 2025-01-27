bfe03b7ee8ff642128fd837a9d9f6959a17c9b89
190101101 <190101101@mail.ru>
2025-01-27 14:53:34 +0400

    50 Vector3Static Methodlar 1

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/50 Vector3Static Methodlar 1/Script/transformislemleri.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformislemleri : MonoBehaviour
+{
+    // static methods
+    public Transform hedef1;
+    public Transform hedef2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        /*
+        // distance
+        float fark = Vector3.Distance(hedef1.position, transform.position);
+        Debug.Log("fark shudur: "+fark);
+        */ 
+
+        /*
+        // angle
+        Vector3 mesafe = hedef1.position - transform.position;
+        float angle = Vector3.Angle(mesafe, transform.forward);
+        float angle = Vector3.Angle(mesafe, transform.up);
+        Debug.Log(angle);
+
+        if(angle < 5f)
+        {
+            Debug.Log("aci saglandi");
+        }
+        */ 
+
+    }
+}
