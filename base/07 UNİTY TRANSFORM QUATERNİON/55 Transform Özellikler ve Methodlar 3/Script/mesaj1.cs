d7a4b72b90dfb9c0daf0aecd1b4dfb06932e8f85
190101101 <190101101@mail.ru>
2025-01-28 22:57:18 +0400

    56 Transform Özellikler ve Methodlar 4

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/55 Transform Özellikler ve Methodlar 3/Script/mesaj1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mesaj1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        gameObject.SendMessage();
+        gameObject.BroadcastMessage();   
+        */
+        // gameObject.SendMessage("ekranabas", "pepi", SendMessageOptions.DontRequireReceiver);
+        gameObject.SendMessage("ekranabas", "pepi");
+        gameObject.BroadcastMessage("ekranabas2", "pepi");
+    }
+
+    void ekranabas(string name)
+    {
+        Debug.Log("hello " + name);
+    }
+
+    void ekranabas2(string name)
+    {
+        Debug.Log("ana obje: hello 2 " + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ base/07 UNİTY TRANSFORM QUATERNİON/55 Transform Özellikler ve Methodlar 3/Script/transformislemleri.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformislemleri : MonoBehaviour
+{
+    // static methods
+    // public Transform hedef1;
+    // public Transform hedef2;
+
+    void Start()
+    {
+        /*
+        GameObject aramasonu = transform.Find("GameObject").gameObject;
+        GameObject aramasonu = transform.Find("GameObject/Cube").gameObject;
+        aramasonu.name="ben degishtirdim";
+        
+        Debug.Log(transform.childCount);
+        Debug.Log(transform.hierarchyCount);
+        */ 
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Debug.Log(transform.GetChild(i).name);
+
+            if(transform.GetChild(i).name == "Capsule")
+            {
+                transform.GetChild(i).GetComponent<CapsuleCollider>().enabled = false;
+            }
+
+            /*
+            if(i == 1)
+            {
+                transform.GetChild(i).GetComponent<CapsuleCollider>().enabled = false;
+            }
+            */ 
+
+        }
+
+
+        /*
+        print(hedef1.transform.localPosition);
+        print(hedef1.transform.localScale);
+
+        transform.position = new Vector3(2f, 3f, 5f);
+        
+        transform.position = new Vector3(
+            transform.position.x, 
+            transform.position.y+3f, 
+            transform.position.z
+        );
+        */ 
+
+        /*
+        hedef1.transform.Rotate(90f,0f,2f, Space.Self);
+        hedef2.transform.Rotate(90f,0f,2f, Space.World);
+        */ 
+    }
+
+    void Update()
+    {
+        /*
+        hedef2.transform.LookAt(hedef1);
+        hedef2.transform.LookAt(hedef1,Vector3.left);
+        */ 
+
+        /*
+        hedef1.transform.Rotate(2f,4f,1f, Space.Self);
+        hedef2.transform.Rotate(2f,4f,2f, Space.World);
+        */ 
+
+        /*
+        transform.Rotate(new Vector3(0f,0f,1f), Space.World);
+        transform.Rotate(new Vector3(0f,0f,1f), Space.Self);
+        transform.Rotate(new Vector3(0f,0f,1f) * 1f);
+        */ 
+
+        /*
+        transform.Translate(new Vector3(0f, 0.1f, 0f));
+        transform.Translate(transform.forward * Time.deltaTime * 1f);
+        */ 
+    }
+}
