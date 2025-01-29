2d2523ee1e17b3c9edc6687b6425e181970eaa50
190101101 <190101101@mail.ru>
2025-01-29 21:23:08 +0400

    20 Console Penceresi Anlatım

---
+++ base/04 UNİTY ARAYÜZ TANITIMLARI/20 Console Penceresi Anlatım/Script/Console01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console01 : MonoBehaviour
+{
+    string a;
+    int b;
+    float c;
+    
+    void Start()
+    {
+        Debug.Log(a);
+        Debug.Log(b);
+        Debug.Log(c);
+        Debug.LogError("error");
+        Debug.LogWarning("warning");
+    }
+
+    void Update()
+    {
+        
+    }
+}
