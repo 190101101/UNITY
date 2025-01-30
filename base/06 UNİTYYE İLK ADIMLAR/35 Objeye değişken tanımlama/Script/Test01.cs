cc1adf4aeb6b3a7a8d8e4330f921da76dfca4beb
190101101 <190101101@mail.ru>
2025-01-31 02:06:48 +0400

    35 Objeye değişken tanımlama

---
+++ base/06 UNİTYYE İLK ADIMLAR/35 Objeye değişken tanımlama/Script/Test01.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public string name;
+    public int age;
+    public float salary;
+    private string country;
+    public bool carRights;
+
+    void Start()
+    {
+        country = name;
+        Debug.Log("name:"+name);
+        Debug.Log("age:"+age);
+        Debug.Log("salary:"+salary);
+        Debug.Log("country:"+country);
+        Debug.Log("carRights:"+carRights);
+
+        if(age > 1)
+        {
+            carRights = true;
+            salary = 3000;        
+        }
+
+        if(carRights)
+        {
+            print("have a car right");
+        }
+        else
+        {
+            print("no have a car right");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
