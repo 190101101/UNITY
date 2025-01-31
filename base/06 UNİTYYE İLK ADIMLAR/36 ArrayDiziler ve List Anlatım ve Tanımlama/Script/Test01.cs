c723512fc2e9925ca6d549715d2cdbde30c0b5f7
190101101 <190101101@mail.ru>
2025-01-31 04:16:24 +0400

    36 ArrayDiziler ve List Anlatım ve Tanımlama

---
+++ base/06 UNİTYYE İLK ADIMLAR/36 ArrayDiziler ve List Anlatım ve Tanımlama/Script/Test01.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public string[] childs;
+    public List<string> parents;
+
+    void Start()
+    {
+        foreach(string child in childs)
+        {
+            print("name: " + child);
+        }
+
+        print("the cat name is " + childs[0]);
+
+        parents.Add("kuki");
+    }
+
+    void Update()
+    {
+        
+    }
+}
