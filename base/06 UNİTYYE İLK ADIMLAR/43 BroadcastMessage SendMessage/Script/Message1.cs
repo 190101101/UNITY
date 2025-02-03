e8f6546483b3fa0733432918f7561bc2e51bc70b
190101101 <190101101@mail.ru>
2025-02-03 05:38:53 +0400

    43 BroadcastMessage SendMessage

---
+++ base/06 UNİTYYE İLK ADIMLAR/43 BroadcastMessage SendMessage/Script/Message1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message1 : MonoBehaviour
+{
+    void Start()
+    {
+        /*
+        gameObject.SendMessage();
+        gameObject.BroadcastMessage();
+        */ 
+
+        // SendMessageOptions.DontRequireReceiver text = false;
+        // gameObject.SendMessage("click", "pepi", SendMessageOptions.DontRequireReceiver);
+        gameObject.SendMessage("click");
+        gameObject.BroadcastMessage("click2");
+    }
+
+    void click()
+    {
+        print("clicked");
+    }
+
+    void click2()
+    {
+        print("clicked 2");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ base/06 UNİTYYE İLK ADIMLAR/43 BroadcastMessage SendMessage/Script/Message2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message2 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void click2()
+    {
+        print("msg clicked 2");
+    }
+
+    void Update()
+    {
+        
+    }
+}
