3e33fea1ede80ace504455b90eff5f6cff2c8221
190101101 <190101101@mail.ru>
2025-02-03 05:51:48 +0400

    44 Gameobject DestroyComponent Destroy

---
+++ base/06 UNİTYYE İLK ADIMLAR/44 Gameobject DestroyComponent Destroy/Script/Test01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    void Start()
+    {
+        // Destroy(gameObject, 2f);
+        // Destroy(this); script file silmek ucun.
+        Destroy(GetComponent<CapsuleCollider>(), 2f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
