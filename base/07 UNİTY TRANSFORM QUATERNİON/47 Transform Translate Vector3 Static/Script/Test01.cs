27ce41d64c819abbd8ab78925525dd2732276e98
190101101 <190101101@mail.ru>
2025-02-04 08:35:51 +0400

    47

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/47 Transform Translate Vector3 Static/Script/Test01.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    void Start()
+    {
+        print("position: " + transform.position);
+        print("rotation: " + transform.rotation);
+        print("localScale: " + transform.localScale);
+        transform.Translate(new Vector3(1f, 1f, 1f));
+    }
+
+    void Update()
+    {
+        // print("position: " + transform.position);
+
+    }
+}
---
+++ unity/method/method 47.cs
@@ -0,0 +1,4 @@
+Vector2 a; // v2 2d
+Vector3 b; // v3 3d
+Vector4 c; // v4 bu ise kolgelendirme
+Quaternion d; // iki sheyin arasindaki mesafe
