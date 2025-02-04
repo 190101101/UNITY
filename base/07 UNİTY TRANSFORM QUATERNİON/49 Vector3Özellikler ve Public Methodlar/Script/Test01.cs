ac63bf6e209ea9aee045b04a1ccad7f37477c510
190101101 <190101101@mail.ru>
2025-02-04 11:27:58 +0400

    49

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/49 Vector3Özellikler ve Public Methodlar/Script/Test01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    Vector3 p1;
+    Vector3 p2;
+
+    void Start()
+    {
+        p1 = new Vector3(1f, 1f, 1f);
+        p2 = new Vector3(1f, 1f, 1f);
+
+        print(p1.ToString());
+    }
+
+    void Update()
+    {
+        // print(Vector3.Magnitude(transform.position));
+        // print(transform.position.normalized);
+        print(transform.position.normalized);
+
+    }
+}
---
+++ base/07 UNİTY TRANSFORM QUATERNİON/49 Vector3Özellikler ve Public Methodlar/method 49.cs
@@ -0,0 +1,10 @@
+// Magnitude - vector [position] gelmish oldugumuz pos total uzunluq return
+Vector3.Magnitude(transform.position);
+
+// eyni shey amma kvadrati hesablayir
+print(Vector3.SqrMagnitude(transform.position));
+
+// ferqli variantlarda istifade etmek olar 
+// equals yerine bu operator == daha deqiqdi
+Vector3.Equals(p1, p2);
+Equals(p1, p2);
