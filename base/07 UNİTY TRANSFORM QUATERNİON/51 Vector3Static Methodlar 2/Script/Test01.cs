b6737cc099b5065a4b55bc92b183ce1df32f3ae0
190101101 <190101101@mail.ru>
2025-02-07 03:41:17 +0400

    51

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/51 Vector3Static Methodlar 2/Script/Test01.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public Transform aim1;
+    public Transform aim2;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+
+        /*
+        Time.time
+        Time.deltaTime
+        Time.timeScale
+        */ 
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(aim1.position, aim2.position, (Time.time - startTime) * .2f);
+    }
+}
---
+++ unity/method/method 51.cs
@@ -0,0 +1,8 @@
+// normal time
+Time.time 
+
+// mill second
+Time.deltaTime
+
+// zamani .5 defe azaltmaq
+Time.timeScale * .5f;
