c3fc97df6ff14616f139b22f66acc206b64861fd
190101101 <190101101@mail.ru>
2025-02-07 03:13:21 +0400

    50

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/50 Vector3Static Methodlar 1/Script/Test01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public Transform aim1;
+    public Transform aim2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float difference = Vector3.Distance(aim1.position, transform.position);
+        print("difference: " +difference); 
+
+    }
+}
---
+++ base/07 UNİTY TRANSFORM QUATERNİON/50 Vector3Static Methodlar 1/Script/Test02.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    public Transform aim1;
+    public Transform aim2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 distance = aim1.position - transform.position;
+        float angle = Vector3.Angle(distance, transform.forward);
+        Debug.Log(angle);
+
+        if(angle < 5f)
+        {
+            print("done");
+        }
+    }
+}
