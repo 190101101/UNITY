c6f2761252b91ebded36f78af70e732d87a36edc
190101101 <190101101@mail.ru>
2025-02-07 09:12:12 +0400

    52

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/52 Vector3Static Methodlar 3/Script/Test01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 pos1 = new Vector3(1f, 2f, 5f);
+        Vector3 pos2 = new Vector3(2f, 5f, 1f);
+
+        print(pos1 - pos2);
+        print(pos1 + pos2);
+        print(pos1 / 2f);
+        print(pos1 * 2f);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ base/07 UNİTY TRANSFORM QUATERNİON/52 Vector3Static Methodlar 3/Script/Test02.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 pos1 = new Vector3(1f, 2f, 5f);
+        Vector3 pos2 = new Vector3(2f, 5f, 1f);
+
+        print("Max: " + Vector3.Max(pos1,pos2));
+        print("Min: " + Vector3.Min(pos1,pos2));
+        print("Scale: " + Vector3.Scale(pos1,pos2));
+    }
+
+    void Update()
+    {
+
+    }
+}
