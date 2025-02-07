1af900740d70f6794121e630bfbc63370fea2dfe
190101101 <190101101@mail.ru>
2025-02-08 01:03:37 +0400

    54

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/54 Transform Özellikler ve Methodlar 2/Script/Test01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public Transform aim1;
+    public Transform aim2;
+    
+    void Start()
+    {
+        print(aim1.transform.localPosition);
+        print(aim1.transform.localRotation);
+        print(aim1.transform.localScale);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ base/07 UNİTY TRANSFORM QUATERNİON/54 Transform Özellikler ve Methodlar 2/Script/Test02.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    public Transform aim1;
+    public Transform aim2;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        aim2.transform.LookAt(aim1);
+        aim2.transform.LookAt(aim1, Vector3.left);
+    }
+}
