b506356ffcdca7900b85cfa0dedc0c1f52c9ad8f
190101101 <190101101@mail.ru>
2025-02-08 06:17:29 +0400

    56

---
+++ base/07 UNİTY TRANSFORM QUATERNİON/56 Transform Özellikler ve Methodlar 4/Script/Test01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    public Transform parent;
+    public GameObject child;
+    
+    void Start()
+    {
+        child.transform.SetParent(parent);
+        // child position = false
+        // child.transform.SetParent(parent, false); 
+    }
+
+    void Update()
+    {
+        
+    }
+}
