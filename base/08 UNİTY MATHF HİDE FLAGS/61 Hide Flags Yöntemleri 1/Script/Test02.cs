17421d55607c295be27e1c317aec028c1e866ae2
190101101 <190101101@mail.ru>
2025-02-11 14:27:17 +0400

    61

---
+++ base/08 UNİTY MATHF HİDE FLAGS/61 Hide Flags Yöntemleri 1/Script/Test02.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    public int age;
+    [HideInInspector]
+    public string name;
+
+    void Start()
+    {
+        // gameObject.hideFlags = HideFlags.HideInHierarchy;
+        // gameObject.hideFlags = HideFlags.HideInInspector;
+        // gameObject.hideFlags = HideFlags.NotEditable;
+        gameObject.hideFlags = HideFlags.DontSave;
+    }
+
+    void Update()
+    {
+
+    }
+}
+
---
+++ base/08 UNİTY MATHF HİDE FLAGS/61 Hide Flags Yöntemleri 1/Script/Test03.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test03 : MonoBehaviour
+{
+    public Color name;
+    public Texture name2;
+    public AudioClip name3;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
