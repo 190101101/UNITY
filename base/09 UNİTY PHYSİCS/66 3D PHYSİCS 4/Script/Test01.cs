675e98dd40ac3fc8f990dbaf5eb44c297498423c
190101101 <190101101@mail.ru>
2025-02-14 09:27:06 +0400

    66

---
+++ base/09 UNİTY PHYSİCS/66 3D PHYSİCS 4/Script/Test01.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // if(other.CompareTag("2"))
+        if(other.gameObject.name == "2")
+        {
+            // Destroy(gameObject);
+            Destroy(other.gameObject);
+            print("interaction have");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "2")
+        {
+            print("interaction it continues");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "2")
+        {
+            print("interaction finish");
+        }
+    }
+}
