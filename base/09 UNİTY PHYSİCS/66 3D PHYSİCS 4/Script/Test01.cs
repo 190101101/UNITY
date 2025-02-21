3e8db8447d3f650eb8b84fa1eac6db1aeb791acf
190101101 <190101101@mail.ru>
2025-02-21 14:11:33 +0400

    66 3D PHYSİCS 4

---
+++ base/09 UNİTY PHYSİCS/66 3D PHYSİCS 4/Script/Test01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		// Destroy(other.gameObject);
+		Destroy(gameObject);
+		// other.CompareTag("Enemy");
+		print("enter: "+other.gameObject.name);
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		print("stay: "+other.gameObject.name);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		print("exit: "+other.gameObject.name);
+	}
+}
---
+++ base/09 UNİTY PHYSİCS/66 3D PHYSİCS 4/Script/Test02.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		print("enter: "+collision.gameObject.name);
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		print("stay: "+collision.gameObject.name);
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		print("exit: "+collision.gameObject.name);
+	}
+}
